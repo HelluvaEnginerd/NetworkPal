60e0f2f2e1feb3032c6e12e4b04c26071762d674
Devon Dunmire <ddunmire@mymail.mines.edu>
2017-03-20 21:23:46 -0600

    added some new classes

---
+++ app/src/main/java/com/csci448/goldenrush/networkingpal/database/ApplicationBaseHelper.java
@@ -0,0 +1,39 @@
+package com.csci448.goldenrush.networkingpal.database;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.csci448.goldenrush.networkingpal.Application;
+
+/**
+ * Created by ddunmire on 3/20/2017.
+ */
+
+public class ApplicationBaseHelper extends SQLiteOpenHelper{
+    private static final int VERSION = 1;
+    private static final String DATABASE_NAME = "applicationBase.db";
+
+    public ApplicationBaseHelper(Context context){
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + ApplicationDbSchema.ApplicationTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                ApplicationDbSchema.ApplicationTable.Cols.NAME + ", " +
+                ApplicationDbSchema.ApplicationTable.Cols.TITLE + ", " +
+                ApplicationDbSchema.ApplicationTable.Cols.CONTACT + ", " +
+                ApplicationDbSchema.ApplicationTable.Cols.DATE + ", " +
+                ApplicationDbSchema.ApplicationTable.Cols.COMPANY + ", " +
+                ApplicationDbSchema.ApplicationTable.Cols.UUID +
+                ")"
+        );
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
+
+    }
+}
