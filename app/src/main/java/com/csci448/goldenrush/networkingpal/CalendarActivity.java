96f97e3b370625bfe268098663b7bcad6db467a0
Hayden Scott <hascott@mymail.mines.edu>
2017-05-08 15:42:26 -0600

    dope new calendar yo check it

--- app/src/main/java/com/csci448/goldenrush/networkingpal/CalendarActivity.java
+++ app/src/main/java/com/csci448/goldenrush/networkingpal/CalendarActivity.java
@@ -3,34 +3,23 @@ package com.csci448.goldenrush.networkingpal;
 import android.content.Context;
 import android.content.Intent;
 
-import android.content.res.Configuration;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 
-import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.CalendarView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.text.SimpleDateFormat;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Sarah on 2/28/2017.
@@ -41,7 +30,9 @@ public class CalendarActivity extends AppCompatActivity {
     private static final String TAG = "Calendar Activity";
     private static final String EXTRA_DATE = "extra_date";
     FloatingActionButton newEventButton;
-    CalendarView calendarWidget;
+    private Calendar currentCalender = Calendar.getInstance(Locale.getDefault());
+    //CalendarView calendarWidget;
+    CompactCalendarView mCompactCalendarView;
     private TextView currentDateText;
     private String currentDateString;
 
@@ -52,12 +43,13 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         Log.d(TAG, "onCreate Called");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calendar_activity);
+        //setContentView(R.layout.calendar_activity);
+        setContentView(R.layout.compact_calendar_view);
 
         setUpWidgets();
         if(savedInstanceState!=null)
         {
-            calendarWidget.setDate(savedInstanceState.getLong(DATE));
+            //calendarWidget.setDate(savedInstanceState.getLong(DATE));
         }
         //create the list of event fragments
         FragmentManager fm = getSupportFragmentManager();
@@ -74,8 +66,14 @@ public class CalendarActivity extends AppCompatActivity {
     private void setUpWidgets(){
         Log.d(TAG, "setupWidgets()");
         newEventButton = (FloatingActionButton) findViewById(R.id.fab_add_event);
-        calendarWidget = (CalendarView) findViewById(R.id.calendar_widget);
-
+        mCompactCalendarView = (CompactCalendarView) findViewById(R.id.compactcalendar_view);
+        mCompactCalendarView.setCalendarBackgroundColor(Color.DKGRAY);
+        mCompactCalendarView.setFirstDayOfWeek(Calendar.MONDAY);
+        List<Event> events = EventLab.get(getApplicationContext()).getEvents();
+        for (Event e: events){
+            mCompactCalendarView.addEvent(new com.github.sundeepk.compactcalendarview.domain.Event(Color.GREEN, e.getmEventDate().getTime()));
+        }
+        // calendarWidget = (CalendarView) findViewById(R.id.calendar_widget);
         newEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,9 +84,10 @@ public class CalendarActivity extends AppCompatActivity {
         });
 
         currentDateText= (TextView) findViewById(R.id.events_on_date);
-        currentDateString = new SimpleDateFormat("MM/dd/yyyy").format(new Date(calendarWidget.getDate()));
+        //currentDateString = new SimpleDateFormat("MM/dd/yyyy").format(new Date(calendarWidget.getDate()));
         currentDateText.setText(currentDateString);
 
+        /*
         //change the date showing
         calendarWidget.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
@@ -98,6 +97,18 @@ public class CalendarActivity extends AppCompatActivity {
                 currentDateText.setText(currentDateString);
 
                 //update the list of items based off of date
+            }
+        });
+        */
+        //set title on calendar scroll
+        mCompactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<com.github.sundeepk.compactcalendarview.domain.Event> bookingsFromMap = mCompactCalendarView.getEvents(dateClicked);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
             }
         });
 
@@ -113,10 +124,19 @@ public class CalendarActivity extends AppCompatActivity {
         return i ;
     }
 
+    @Override
+    public void onResume(){
+        super.onResume();
+        mCompactCalendarView.removeAllEvents();
+        List<Event> events = EventLab.get(getApplicationContext()).getEvents();
+        for (Event e: events){
+            mCompactCalendarView.addEvent(new com.github.sundeepk.compactcalendarview.domain.Event(Color.GREEN, e.getmEventDate().getTime()));
+        }
+    }
+
     public void onSaveInstanceState(Bundle savedInstanceState) {
         Log.d(TAG, "onSaveInstanceStateCalled");
         super.onSaveInstanceState(savedInstanceState);
-        savedInstanceState.putLong(DATE, calendarWidget.getDate());
-
+        //savedInstanceState.putLong(DATE, calendarWidget.getDate());
     }
 }
